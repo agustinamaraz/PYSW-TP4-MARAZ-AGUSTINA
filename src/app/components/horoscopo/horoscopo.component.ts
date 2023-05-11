33df87ddd0bed3c067d3429fba91261617ee478c
Agustina <102815348+agustinamaraz@users.noreply.github.com>
2023-05-11 15:58:05 -0300

    commit

--- src/app/components/horoscopo/horoscopo.component.ts
+++ src/app/components/horoscopo/horoscopo.component.ts
@@ -6,21 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./horoscopo.component.css']
 })
 export class HoroscopoComponent {
-  signs:Array<any>;
   signos!:Array<any>;
 
   constructor() { 
-    this.signs = [
-      {name:'leo',img:'team5.jpg'},
-      {name:'aries',img:'team5.jpg'},
-      {name:'sagitario',img:'team5.jpg'},
-      {name:'acuario',img:'team5.jpg'},
-      {name:'tauro',img:'team5.jpg'},
-      {name:'geminis',img:'team5.jpg'},
-      {name:'libra',img:'team5.jpg'},
-      {name:'escorpio',img:'team5.jpg'},
-      {name:'cancer',img:'team5.jpg'}
-    ]
+
   }
 
   ngOnInit(): void {
